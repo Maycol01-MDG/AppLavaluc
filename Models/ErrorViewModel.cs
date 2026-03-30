1cf9c6653487682e38eb92881ebb0d231121b43a
Your Name <you@example.com>
2026-03-30 16:07:30 -0500

    Limpieza: eliminar duplicados y archivos no usados

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AppLavaluc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
